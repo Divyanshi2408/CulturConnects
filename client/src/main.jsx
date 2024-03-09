83f6d35daac188031f784aa9cdcaec87872ef3f7
Divyanshi <divynashipal2808@gmail.com>
2024-03-10 00:03:48 +0530

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-1lxg3fk71kaqu6it.us.auth0.com"
      clientId="FDG4dk98FVpoGMnFyOPdmefqG4ChdYDO"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://cultur-connects.vercel.app"
      }}
      audience="https://dev-1lxg3fk71kaqu6it.us.auth0.com/api/v2/"
      scope="openid profile email"
